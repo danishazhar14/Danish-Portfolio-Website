6b7253a7e61a9914d21ad9da8ccde925e28e9623
Danish Azhar <daniazhar14@gmail.com>
2025-05-24 08:45:37 +0500

    Add files via upload

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPage from '../components/MainPage';
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <MainPage />
+    </div>
+  );
+};
+
+export default Home;
